5e088f2961c6cb53316bc6c391dd2a52ea6df14f
Nathan Sprague <nathan.r.sprague@gmail.com>
2022-10-03 16:39:36 -0400

    update setup.py with dependencies and fix deprecation warning for shapely.

--- setup.py
+++ setup.py
@@ -9,5 +9,11 @@ setup(
     author='Nathan Sprague',
     author_email='nathan.r.sprague@gmail.com',
     description='Simple 2D Arm Simulator',
+    install_requires=[
+        'setuptools>=46.1.3',
+        'numpy>=1.13.3',
+        'Shapely>=1.7.0',
+        'matplotlib>=2.1.1',
+    ],
     scripts=['scripts/arm_animation.py']
 )
